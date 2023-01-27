6dd7cf6d153c63e6e5b82e15916a4114f25b72ed
Philip Turner <philipturner.AR@gmail.com>
2023-01-26 20:58:46 -0500

    Working on build script

--- build.sh
+++ build.sh
@@ -17,10 +17,9 @@ except ImportError:
 f = open("python_messaging.txt", "w")
 f.write(message)
 f.close()
-' >> "python_script.py"
+' > "python_script.py"
 python3 python_script.py
 
-# Fail if not installed.
 if [[ "success" != `cat python_messaging.txt` ]]; then
     echo "OpenMM not installed."
     exit -1
@@ -31,10 +30,47 @@ echo '
 import openmm
 
 f = open("python_messaging.txt", "w")
-f.write(openmm.__version__)
+f.write(openmm.version.version)
 f.close()
-' >> "python_script.py"
+' > "python_script.py"
 python3 python_script.py
 
-# Fail if too early.
-echo `cat python_messaging.txt`
+openmm_version=`cat python_messaging.txt`
+old_IFS=$IFS
+IFS='.'
+read -a strarr <<< "$openmm_version"
+openmm_version_major=$((strarr[0] + 0)) # Force convert to integer.
+IFS=$old_IFS
+
+# TODO: Check version minor when HIP workaround is patched. Also incorporate
+# this check into the installer script.
+if [[ $openmm_version_major -lt 8 ]]; then
+    echo "OpenMM must be version 8.0.0 or higher."
+    exit -1
+fi
+
+# Extract OpenMM install location.
+# WARNING: The machine building this must link against the same location as you
+# would expect on client machines.
+echo '
+import openmm
+import os
+
+path = openmm.version.openmm_library_path
+path = os.path.normpath(path)
+path, ignored = os.path.split(path)
+path = path.replace(os.path.expanduser("~"), "~", 1)
+
+f = open("python_messaging.txt", "w")
+f.write(path)
+f.close()
+' > "python_script.py"
+python3 python_script.py
+
+openmm_parent_dir=`cat python_messaging.txt`
+openmm_lib_dir="${openmm_parent_dir}/lib"
+openmm_include_dir="${openmm_parent_dir}/include"
+
+# TODO: Use 'lipo' to merge both binaries into one file.
+# https://developer.apple.com/documentation/apple-silicon/building-a-universal-macos-binary
+# This doubles shader size and inline shader strings, but it's fine...
